9872e2553dba2815c745fe869c28b15a5197e89e
drRamzywork <drRamzyworkl@gmail.com>
2025-05-27 06:37:01 +0300

    fix home page topics image

--- src/components/Home/HowTo2/index.jsx
+++ src/components/Home/HowTo2/index.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
-import styles from './index.module.scss';
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import styles from "./index.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 const HowTo2 = ({ dataPostWudoo, dir, dataPostPray }) => {
-
   return (
     <>
-      <section id='how_to' className={styles.how_to} dir={dir}>
+      <section id="how_to" className={styles.how_to} dir={dir}>
         <div className={styles.sec_container}>
           <div className={styles.cards_container}>
-            <Link href={`/preliminaries/${dataPostWudoo?.slug}`} className={styles.card}>
+            <Link
+              href={`/preliminaries/${dataPostWudoo?.slug}`}
+              className={styles.card}
+            >
               <div className={styles.img_container}>
-                <Image width={372} height={231} src={dataPostWudoo.image} alt="" />
+                <Image
+                  width={372}
+                  height={231}
+                  src={dataPostWudoo?.image}
+                  alt=""
+                />
               </div>
               <div className={styles.title}>
                 <h4>{dataPostWudoo?.title}</h4>
               </div>
             </Link>
 
-            <Link href={`/preliminaries/${dataPostPray?.slug}`} className={styles.card}>
+            <Link
+              href={`/preliminaries/${dataPostPray?.slug}`}
+              className={styles.card}
+            >
               <div className={styles.img_container}>
-                <Image width={372} height={231} src={dataPostPray.image} alt="" />
+                <Image
+                  width={372}
+                  height={231}
+                  src={dataPostPray?.image}
+                  alt=""
+                />
               </div>
               <div className={styles.title}>
                 <h4>{dataPostPray?.title}</h4>
@@ -31,12 +46,11 @@ const HowTo2 = ({ dataPostWudoo, dir, dataPostPray }) => {
                 <img src="/assets/svgs/ArrowRight.svg" alt="" />
               </div>
             </Link>
-
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default HowTo2
+export default HowTo2;
